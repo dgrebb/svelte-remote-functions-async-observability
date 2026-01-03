d769db99c11332435bbe58c92770b38f43a664c2
Dan Grebb <445891+dgrebb@users.noreply.github.com>
2026-01-03 16:37:46 -0500

    feat(vite): add Rollup visualizer plugin for build statistics reporting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
+import { visualizer } from 'rollup-plugin-visualizer';
+
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import fs from 'fs';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()],
+	plugins: [
+		sveltekit(),
+		devtoolsJson(),
+		visualizer({
+			emitFile: true,
+			filename: '.reports/build/vite-rollup-stats.html'
+		}) as PluginOption
+	],
 	server: {
 		https: {
 			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
